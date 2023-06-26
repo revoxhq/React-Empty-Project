87b3fbd3d757f3b7c770e5bde2d44b0478f3ba61
ashangrajapaksha <ashanrajapaksha954@gmail.com>
2023-06-26 10:48:25 +0530

    add sass

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+import "bootstrap/dist/css/bootstrap.css";
+
+const App = () => {
+  return (
+    <div className="d-flex justify-content-center">
+      <h1>React Empty App</h1>
+      <button className="btn btn-primary">Primary</button>
+    </div>
+  );
+};
+
+export default App;
